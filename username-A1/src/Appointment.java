b98362e5cd438539a3a3a81e0bba7c9093d06cb3
xiamojixia <2206486730@qq.com>
2024-11-20 17:41:37 +0800

    Appointment and patient information

---
+++ username-A1/src/Appointment.java
@@ -0,0 +1,31 @@
+public class Appointment {
+    private int id ;
+    private String name ;
+    private String mobilephone ;
+    private String preferredtime ;
+    private String doctortype ;
+
+    public Appointment() {
+        this.id = 0;
+        this.name = "None";
+        this.mobilephone = "None";
+        this.preferredtime = "None";
+        this.doctortype = "None";
+    }
+
+    public Appointment(int id, String name,String mobilephone, String preferredtime, String doctortype) {
+        this.id = id;
+        this.name = name;
+        this.mobilephone = mobilephone;
+        this.preferredtime = preferredtime;
+        this.doctortype = doctortype;
+    }
+
+    public void printAt() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Mobile phone: " + mobilephone);
+        System.out.println("Preferred time: " + preferredtime);
+        System.out.println("Doctor type: " + doctortype);
+    }
+}
